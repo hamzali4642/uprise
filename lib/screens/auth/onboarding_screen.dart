b76cd5b8c21dd1511599029717aa38fc17162da5
M Ahmed <razaraza3536@gmail.com>
2023-08-16 10:14:53 +0500

    scaffold color

--- lib/screens/auth/onboarding_screen.dart
+++ lib/screens/auth/onboarding_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:uprise/widgets/custom_asset_image.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({Key? key}) : super(key: key);
@@ -6,9 +7,10 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Column(
         children: [
-
+          // CustomAssetImage(height: height, path: path)
         ],
       ),
     );
--- lib/screens/auth/splash.dart
+++ lib/screens/auth/splash.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:uprise/generated/assets.dart';
 import 'package:uprise/screens/dashboard.dart';
+import 'package:uprise/widgets/custom_asset_image.dart';
 import 'package:utility_extensions/utility_extensions.dart';
 
 class Splash extends StatelessWidget {
@@ -8,11 +11,19 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     goNext(context);
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+          child: SvgPicture.asset(
+        Assets.imagesURNavigationIcon,
+        width: 200,
+        height: 200,
+      )),
+    );
   }
 
   goNext(BuildContext context) async {
     await Future.delayed(const Duration(seconds: 1));
+    // ignore: use_build_context_synchronously
     context.pushAndRemoveUntil(child: const Dashboard());
   }
 }
