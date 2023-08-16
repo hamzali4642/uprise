97b8a5ef7e08c7c338138192e86693fbd2e581c0
M Ahmed <razaraza3536@gmail.com>
2023-08-16 09:49:11 +0500

    widgets directory custom asset image

---
+++ lib/widgets/custom_asset_image.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomAssetImage extends StatelessWidget {
+  final String path;
+  final double? width, height;
+  final bool reduce;
+  final Color? color;
+  final BoxFit? fit;
+
+  const CustomAssetImage({
+    super.key,
+    required this.height,
+    this.reduce = true,
+    this.width,
+    this.color,
+    required this.path,
+    this.fit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      fit: fit,
+      image: AssetImage(path),
+      width: width ,
+      height: height,
+      // width: width - (reduce ? 25 : 0),
+      // height: height - (reduce ? 25 : 0),
+      color: color,
+    );
+  }
+}
