c58a75dd7c768ce2ebd77c134e8323b0a5adf862
Hamza <hamzali4642@gmail.com>
2023-08-16 12:02:10 +0500

    discovery

---
+++ lib/widgets/chip_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:uprise/helpers/textstyles.dart';
+import 'package:utility_extensions/extensions/font_utilities.dart';
+
+import '../helpers/colors.dart';
+
+class ChipWidget extends StatelessWidget {
+  const ChipWidget({super.key, required this.text});
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5,),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5,),
+      decoration: BoxDecoration(
+        color: CColors.URbtnBgColor,
+        borderRadius: BorderRadius.circular(4,),
+      ),
+      child: Text(
+        text,
+        style: AppTextStyles.popins(
+          style: const TextStyle(
+            color: CColors.primary,
+            fontSize: 14,
+            fontWeight: FontWeights.medium,
+          )
+        ),
+      ),
+    );
+  }
+}
