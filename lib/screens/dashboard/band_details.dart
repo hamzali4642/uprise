7651f36ee94ed7f74d6531bd7deedab3e2a1d494
M Ahmed <razaraza3536@gmail.com>
2023-08-18 19:46:32 +0500

    fixs

--- lib/screens/dashboard/band_details.dart
+++ lib/screens/dashboard/band_details.dart
@@ -155,68 +155,73 @@ class _BandDetailsState extends State<BandDetails>
               ],
             ),
           ),
-          Builder(
-            builder: (context) {
-              var uid = FirebaseAuth.instance.currentUser!.uid;
-              var isFollowed = widget.band.followers.contains(uid);
-              return TextButton(
-                onPressed: () {
-                  var my = FirebaseFirestore.instance.collection("users").doc(uid);
-                  var other = FirebaseFirestore.instance.collection("users").doc(widget.band.id);
-                  if(isFollowed){
-                    my.update({
-                    "following" : FieldValue.arrayRemove([other.id]) });
-                    other.update({
-                      "followers" : FieldValue.arrayRemove([my.id]) });
+          Builder(builder: (context) {
+            var uid = FirebaseAuth.instance.currentUser!.uid;
+            var isFollowed = widget.band.followers.contains(uid);
+            return TextButton(
+              onPressed: () {
+                var my =
+                    FirebaseFirestore.instance.collection("users").doc(uid);
+                var other = FirebaseFirestore.instance
+                    .collection("users")
+                    .doc(widget.band.id);
+                if (isFollowed) {
+                  my.update({
+                    "following": FieldValue.arrayRemove([other.id])
+                  });
+                  other.update({
+                    "followers": FieldValue.arrayRemove([my.id])
+                  });
 
-                    widget.band.followers.remove(my.id);
-                    dataProvider.userModel!.following.remove(my.id);
+                  widget.band.followers.remove(my.id);
+                  dataProvider.userModel!.following.remove(my.id);
 
-                    dataProvider.notifyListeners();
-                  }else{
-                    my.update({
-                      "following" : FieldValue.arrayUnion([other.id]) });
-                    other.update({
-                      "followers" : FieldValue.arrayUnion([my.id]) });
+                  dataProvider.notifyListeners();
+                } else {
+                  my.update({
+                    "following": FieldValue.arrayUnion([other.id])
+                  });
+                  other.update({
+                    "followers": FieldValue.arrayUnion([my.id])
+                  });
 
-                    widget.band.followers.add(my.id);
-                    dataProvider.userModel!.following.add(my.id);
+                  widget.band.followers.add(my.id);
+                  dataProvider.userModel!.following.add(my.id);
 
-                    dataProvider.notifyListeners();
-                  }
-                },
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 15,
-                    vertical: 5,
+                  dataProvider.notifyListeners();
+                }
+              },
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 5,
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    20,
                   ),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(
-                      20,
+                  color: CColors.calendarBtnBg,
+                ),
+                child: Row(
+                  children: [
+                    SvgPicture.asset(
+                      Assets.imagesBlackUserAdd,
+                      color: Colors.white,
                     ),
-                    color: CColors.calendarBtnBg,
-                  ),
-                  child: Row(
-                    children: [
-                      SvgPicture.asset(
-                        Assets.imagesBlackUserAdd,
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      isFollowed ? "Unfollow" : "Follow",
+                      style: const TextStyle(
                         color: Colors.white,
                       ),
-                      const SizedBox(
-                        width: 10,
-                      ),
-                      Text(
-                        isFollowed ? "Unfollow" : "Follow",
-                        style: const TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              );
-            }
-          ),
+              ),
+            );
+          }),
         ],
       ),
     );
